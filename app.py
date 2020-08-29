c33b102da7010a80d047c7f29b20d1c3f5af1316
jgregoriods <jonas.gregorio@gmail.com>
2020-08-29 20:02:08 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, make_response
+
+from models.dates import MayaDate
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def home_template():
+    if request.method == 'GET':
+        maya_date = MayaDate()
+
+        gregorian_year = maya_date.gregorian['year']
+        era = 'CE'
+        if maya_date.gregorian['year'] < 0:
+            gregorian_year = abs(gregorian_year) + 1
+            era = 'BCE'
+
+        return render_template('home.html', maya_date=maya_date, gregorian_year=gregorian_year, era=era)
+    else:
+        if 'maya-form' in request.form:
+            maya_date = MayaDate([int(request.form['baktun']),
+                                int(request.form['katun']),
+                                int(request.form['tun']),
+                                int(request.form['uinal']),
+                                int(request.form['kin'])])
+        elif 'gregorian-form' in request.form:
+            year = int(request.form['greg-year'])
+            maya_date = MayaDate({})
+
+        gregorian_year = maya_date.gregorian['year']
+        era = 'CE'
+        if maya_date.gregorian['year'] < 0:
+            gregorian_year = abs(gregorian_year) + 1
+            era = 'BCE'
+
+        return render_template('home.html', maya_date=maya_date, gregorian_year=gregorian_year, era=era)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models/dates.py
@@ -0,0 +1,123 @@
+import datetime
+
+from astropy.time import Time
+from math import floor, ceil
+
+
+tzolkin_names = ['Ahau', 'Imix', 'Ik', 'Akbal', 'Kan', 'Chicchan', 'Cimi',
+                 'Manik', 'Lamat', 'Muluc', 'Oc', 'Chuen', 'Eb', 'Ben', 'Ix',
+                 'Men', 'Cib', 'Caban', 'Etznab', 'Cauac']
+
+haab_names = ['Pop', 'Uo', 'Zip', 'Zotz', 'Tzec', 'Xul', 'Yaxkin', 'Mol',
+              'Chen', 'Yax', 'Zac', 'Ceh', 'Mac', 'Kankin', 'Muan', 'Pax',
+              'Kayab', 'Cumku', 'Uayeb']
+
+
+class MayaDate:
+    def __init__(self, date=[0, 0, 0, 0, 0], format='long_count', coefficient=584283):
+        self.coefficient = coefficient
+
+        if format == 'long_count':
+            self.long_count = date
+            self.julian_day = MayaDate.to_decimal(date) + coefficient
+            self.gregorian = self.get_gregorian()
+            self.julian = self.get_julian()
+        
+        elif format == 'gregorian':
+            self.gregorian = date
+            self.julian_day = ceil(Time(date).jd)
+            self.long_count = self.get_long_count()
+            self.julian = self.get_julian()
+
+        elif format == 'julian':
+            self.julian = date
+            self.julian_day = ceil(Time(date).jd) + MayaDate.julian_offset(date)
+            self.gregorian = self.get_gregorian()
+            self.long_count = self.get_long_count()
+
+        self.tzolkin = self.get_tzolkin()
+        self.haab = self.get_haab()
+
+    def get_gregorian(self):
+        jd = Time(self.julian_day, format='jd')
+        jd.format = 'ymdhms'
+
+        return {
+            'year': jd.value.year,
+            'month': jd.value.month,
+            'day': jd.value.day
+            }
+
+    def get_long_count(self):
+        return MayaDate.to_vigesimal(self.julian_day - self.coefficient)
+
+    def get_julian(self):
+        jd = Time(self.julian_day - MayaDate.julian_offset(self.gregorian), format='jd')
+        jd.format = 'ymdhms'
+
+        return {
+            'year': jd.value.year,
+            'month': jd.value.month,
+            'day': jd.value.day
+            }
+
+    def get_tzolkin(self):
+        n_days = MayaDate.to_decimal(self.long_count)
+        # Adjust for the offset of 4 Ahau
+        coef = (n_days + 3) % 13 + 1
+        name = tzolkin_names[n_days % 20]
+        return [coef, name]
+
+    def get_haab(self):
+        n_days = MayaDate.to_decimal(self.long_count)
+        # Adjust for the offset of 8 Cumku
+        year_day = (n_days + 348) % 365
+        if year_day > 360:
+            coef = year_day - 360
+            name = 'Uayeb'
+        else:
+            coef = year_day % 20
+            name = haab_names[floor(year_day / 20)]
+        return [coef, name]
+
+    def __str__(self):
+        long_count = '.'.join(map(str, self.long_count))
+        tzolkin = ' '.join(map(str, self.tzolkin))
+        haab = ' '.join(map(str, self.haab))
+
+        return f'{long_count} {tzolkin} {haab}'
+
+    @staticmethod
+    def to_vigesimal(decimal):
+        vigesimal = [0, 0, 0, 0, 0]
+
+        vigesimal[0] = decimal // 144000
+        decimal %= 144000
+
+        vigesimal[1] = decimal // 7200
+        decimal %= 7200
+
+        vigesimal[2] = decimal // 360
+        decimal %= 360
+
+        vigesimal[3] = decimal // 20
+        decimal %= 20
+
+        vigesimal[4] = decimal
+
+        return vigesimal
+
+    @staticmethod
+    def to_decimal(vigesimal):
+        multiples = [144000, 7200, 360, 20, 1]
+        decimal = sum(x * y for x, y in zip(vigesimal, multiples))
+        return decimal
+
+    @staticmethod
+    def julian_offset(date):
+        #return ceil(year / 100) - 3 - (year // 400)
+        # Last alignment between Gregorian and Julian Calendars was Feb 1 200
+        n_days = Time(date).jd - Time({'year': 200, 'month': 2, 'day': 1}).jd
+        centuries = n_days // 36524
+        extra_leap = (centuries + 2) // 4
+        return centuries - extra_leap
