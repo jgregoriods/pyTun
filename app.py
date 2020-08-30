c79da89826a2511053719dbc1df88c62963fb9a3
jgregoriods <jonas.gregorio@gmail.com>
2020-08-30 11:55:02 -0300

    trying wtforms

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, redirect
 
 from models.dates import MayaDate
 from models.forms import MayaForm
@@ -6,16 +6,23 @@ from models.forms import MayaForm
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+app.config['SECRET_KEY'] = 'any secret string'
+
+
+@app.route('/')
 def home_template():
-    maya_form = MayaForm()
     maya_date = MayaDate()
-    if request.method == 'POST':
-        maya_date = MayaDate([maya_form.baktun.data,
-                              maya_form.katun.data,
-                              maya_form.tun.data,
-                              maya_form.uinal.data,
-                              maya_form.kin.data])
+  
+    maya_form = MayaForm(obj=maya_date, baktun=maya_date.long_count[0],
+                                        katun=maya_date.long_count[1],
+                                        tun=maya_date.long_count[2],
+                                        uinal=maya_date.long_count[3],
+                                        kin=maya_date.long_count[4],
+                                        
+                                        greg_day=maya_date.gregorian['day'],
+                                        greg_month=maya_date.gregorian['month'],
+                                        greg_year=maya_date.gregorian['year'])
+
     return render_template('home.html', maya_form=maya_form, maya_date=maya_date)
 """
     if request.method == 'GET':
@@ -47,6 +54,21 @@ def home_template():
 
         return render_template('home.html', maya_date=maya_date, gregorian_year=gregorian_year, era=era)
 """
+
+@app.route('/convert', methods=['POST'])
+def convert_template():
+
+    if 'submit_maya' in request.form:
+        maya_date = MayaDate([int(request.form['baktun']),
+                              int(request.form['katun']),
+                              int(request.form['tun']),
+                              int(request.form['uinal']),
+                              int(request.form['kin'])])
+
+        new_maya_form = MayaForm()
+        new_maya_form.greg_year.value = maya_date.gregorian['year']
+
+    return render_template('home.html', maya_form=new_maya_form, maya_date=maya_date)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models/forms.py
+++ models/forms.py
@@ -1,9 +1,18 @@
-from wtforms import Form, IntegerField
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, SubmitField, validators
 
 
-class MayaForm(Form):
-    baktun = IntegerField('baktun')
+class MayaForm(FlaskForm):
+    baktun = IntegerField('baktun', [validators.NumberRange(min=0, max=19)])
     katun = IntegerField('katun')
     tun = IntegerField('tun')
     uinal = IntegerField('uinal')
     kin = IntegerField('kin')
+
+    submit_maya = SubmitField('Convert')
+
+    greg_day = IntegerField('greg_day')
+    greg_month = IntegerField('greg_month')
+    greg_year = IntegerField('greg_year')
+
+    submit_greg = SubmitField('Convert')
